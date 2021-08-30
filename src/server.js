8a368006452ca1467d3b9cad68d3e601383b32d7
Paul Minh Hùng von Kattchee <pmhvk@gmx.de>
2021-08-30 12:54:33 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,31 @@
+// benötigte Pakete einbinden
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+// zu verwendeden Port definieren
+const PORT = process.env.PORT || 3000;
+
+// Server einrichten
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Server starten
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`)
+});
+
+// statische Website bereitstellen
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection' , socket => {    
+    socket.on('message' , msg => {
+        console.log(msg);
+
+        socket.emit('message', 'Hallo Client!');
+        socket.broadcast.emit('message', 'Hallo alle anderen Clients!');
+        socket.emit('message', 'Hallo Welt!');
+    });
+});
